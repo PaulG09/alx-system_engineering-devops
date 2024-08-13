80d6d257565c8398086bc4c19be7a49788c58b78
PaulG09 <lotuds107@gmail.com>
2024-08-13 18:14:35 +0000

    Do hard things

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
 
-"""
-Queries the Reddit API and returns the number of subscribers for a
-given subreddit
-If the subreddit is invalid, the function returns 0.
-"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-
     """
     Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agents": "myApp/0.0.1"}
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    try:
-        # Make the requests with no redirect
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        # Check if the response was successful
-        if response.status_code == 200:
-            data = response.json()
-            return data['data'].get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
-"""
-function that queries the Reddit API
-Prints the first 10 of the hot lists
-"""
 
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-def top_ten_subreddit(subreddit):
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    response = requests.get(url, headers={"User-Agent": "My Reddit Bot"})
-
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-        else:
-            print("None")
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
+        print(None)

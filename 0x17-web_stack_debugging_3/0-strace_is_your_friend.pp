c2ada825b93de2fcaba491d7af95e19ad40fdc65
PaulG09 <lotuds107@gmail.com>
2024-08-15 13:35:00 +0000

    Do hard things

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
-# This Puppet manifest ensures the correct Apache configuration is in place
-file { '/etc/apache2/apache2.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/apache/apache2.conf',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-}
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  require   => File['/etc/apache2/apache2.conf'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
